dbff9d049d459cfb7a57a13afcc2cbc6a1ea68c2
hxpe-dev <316854@vassaux.net>
2025-09-16 17:53:54 +0200

    added lock file + fixed quotes location

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ kotofetch is a small, configurable CLI that displays Japanese quotes in the term
 User config lives in `~/.config/kotofetch/config.toml` and controls display options. Built-in quotes are embedded in the binary; users may place custom quotes in `~/.config/kotofetch/quotes.toml`.
 
 ## Usage
-`kotofetch` - default
+`kotofetch` - default usage  
 `kotofetch --mode haiku --horizontal-padding 3` 
--- src/quotes.rs
+++ src/quotes.rs
@@ -1,4 +1,7 @@
 use serde::Deserialize;
+use std::fs;
+use std::path::PathBuf;
+use dirs::config_dir;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Quote {
@@ -15,33 +18,64 @@ pub struct QuotesCollection {
 }
 
 impl QuotesCollection {
+    fn parse_quotes(s: &str) -> Option<Vec<Quote>> {
+        #[derive(Deserialize)]
+        struct Wrapper {
+            quote: Vec<Quote>,
+        }
+        match toml::from_str::<Wrapper>(s) {
+            Ok(w) => Some(w.quote),
+            Err(e) => {
+                eprintln!("Failed to parse quotes TOML: {}", e);
+                None
+            }
+        }
+    }
+
+    fn parse_file(path: &PathBuf) -> Option<Vec<Quote>> {
+        if path.exists() {
+            match fs::read_to_string(path) {
+                Ok(content) => Self::parse_quotes(&content),
+                Err(e) => {
+                    eprintln!("Failed to read {:?}: {}", path, e);
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+
     pub fn default_with_builtins() -> Self {
-        // parse embedded TOML files
         let mut out = QuotesCollection::default();
 
-        // note: include_str! paths are relative to this file (src/quotes.rs -> ../quotes/...)
+        // --- load built-ins (from repo) ---
         let prov_toml = include_str!("../quotes/proverbs.toml");
         let h_toml = include_str!("../quotes/haiku.toml");
         let a_toml = include_str!("../quotes/anime.toml");
 
-        let parse = |s: &str| -> Option<Vec<Quote>> {
-            // the TOML files are a table array [[quote]]
-            #[derive(Deserialize)]
-            struct Wrapper {
-                quote: Vec<Quote>,
-            }
-            match toml::from_str::<Wrapper>(s) {
-                Ok(w) => Some(w.quote),
-                Err(e) => {
-                    eprintln!("Failed to parse built-in quotes: {}", e);
-                    None
-                }
-            }
-        };
+        out.proverb = Self::parse_quotes(prov_toml);
+        out.haiku = Self::parse_quotes(h_toml);
+        out.anime = Self::parse_quotes(a_toml);
 
-        out.proverb = parse(prov_toml);
-        out.haiku = parse(h_toml);
-        out.anime = parse(a_toml);
+        // --- try to override from ~/.config/kotofetch/quotes ---
+        if let Some(mut cfg_dir) = config_dir() {
+            cfg_dir.push("kotofetch/quotes");
+
+            let prov_path = cfg_dir.join("proverbs.toml");
+            let haiku_path = cfg_dir.join("haiku.toml");
+            let anime_path = cfg_dir.join("anime.toml");
+
+            if let Some(v) = Self::parse_file(&prov_path) {
+                out.proverb = Some(v);
+            }
+            if let Some(v) = Self::parse_file(&haiku_path) {
+                out.haiku = Some(v);
+            }
+            if let Some(v) = Self::parse_file(&anime_path) {
+                out.anime = Some(v);
+            }
+        }
 
         out
     }
