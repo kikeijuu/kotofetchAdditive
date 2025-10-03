2d3d9b014a82c5815735624f53ed9875eb29021f
hxpe-dev <316854@vassaux.net>
2025-10-03 18:05:37 +0200

    Nix bump to latest

--- default.nix
+++ default.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "kotofetch";
-  version = "0.2.16";
+  version = "0.2.17";
 
   src = pkgs.fetchurl {
     url = "https://github.com/hxpe-dev/kotofetch/releases/download/v${version}/kotofetch-v${version}-x86_64-unknown-linux-gnu.tar.gz";
-    sha256 = "sha256-RrA85NTAmWXkhPepo03E0509asjA6Fyp1hCd159L4u0="; # fill in
+    sha256 = "sha256-4MQw4wN9IYzeMC6l+Jns/TMoDGMEzq/vlqGIxNJpcwU="; # fill in
   };
 
   installPhase = ''
